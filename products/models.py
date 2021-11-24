eb2002540dfe8ae9b4235caf0c20b079814c7092
Michael Lenahan <michael_lenahan@hotmail.com>
2021-11-24 21:53:20 +0000

    Add product app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_service = models.BooleanField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
