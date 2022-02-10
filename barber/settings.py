56da034e051ee460f0564d6bbc951bb11157b3ff
Michael Lenahan <michael_lenahan@hotmail.com>
2022-02-10 12:42:58 +0000

    add stripe elements for checkout

--- barber/settings.py
+++ barber/settings.py
@@ -171,4 +171,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-mlenahan-ms5django-rewg158xnyf.ws-eu30.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-mlenahan-ms5django-lhas4inctd7.ws-eu31.gitpod.io']
