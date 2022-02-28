d697ab8ececaba7d15e81b46470eb0d2d88cd8d9
Michael Lenahan <michael_lenahan@hotmail.com>
2022-02-28 13:18:31 +0000

    add choice field to booking form for times

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,27 @@ from .widgets import CustomClearableFileInput
 from .models import Product
 from products.widgets import TimePickerInput, DateTimePickerInput
 
+CHOICES = (
+    (10.00, '10.00'),
+    (10.30, '10.30'),
+    (11.00, '11.00'),
+    (11.30, '11.30'),
+    (12.00, '12.00'),
+    (12.30, '12.30'),
+    (13.00, '13.00'),
+    (13.30, '13.30'),
+    (14.00, '14.00'),
+    (14.30, '14.30'),
+    (15.00, '15.00'),
+    (15.30, '15.30'),
+    (16.00, '16.00'),
+    (16.30, '16.30'),
+    (17.00, '17.00'),
+    (17.30, '17.30'),
+    (18.00, '18.00'),
+    (18.30, '18.30'),
+)
+
 
 class ProductForm(forms.ModelForm):
 
@@ -18,5 +39,4 @@ class ProductForm(forms.ModelForm):
 
 class DateForm(forms.Form):
     date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    my_time_field = forms.TimeField(widget=TimePickerInput)
-    my_date_time_field = forms.DateTimeField(widget=DateTimePickerInput)
+    time_choice = forms.ChoiceField(choices=CHOICES)
