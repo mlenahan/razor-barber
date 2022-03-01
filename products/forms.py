163311a53fb78ca8710ac2e8f45695347306b8fe
Michael Lenahan <michael_lenahan@hotmail.com>
2022-03-01 19:14:52 +0000

    update dateform to render times dynamically

--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .widgets import CustomClearableFileInput
 from .models import Product
 from products.widgets import TimePickerInput, DateTimePickerInput
+from datetime import datetime, timedelta
 
 CHOICES = (
     (10.00, '10.00'),
@@ -33,10 +34,40 @@ class ProductForm(forms.ModelForm):
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
 
 
 class DateForm(forms.Form):
     date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    time_choice = forms.ChoiceField(choices=CHOICES)
+    time_choice = forms.ChoiceField(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        time_field = self.fields['time_choice']
+        today = datetime.today()
+        
+        opening = today.replace(hour=9, minute=0, second=0, microsecond=0)
+        closing = today.replace(hour=17, minute=0, second=0, microsecond=0)
+        delta = timedelta(minutes=30)
+        time_range = datetime_range(opening, closing, delta)
+        choices = []
+        for dt in time_range:
+            value = dt.time
+            display = dt.strftime('%H:%M')
+            choice = [value, display]
+            choices.append(choice)
+        time_field.choices = choices
+
+
+    
+
+# dts = [dt.strftime('%Y-%m-%d T%H:%M Z') for dt in 
+#        datetime_range(datetime(2016, 9, 1, 7), datetime(2016, 9, 1, 9+12), 
+#        timedelta(minutes=15))]
+
+# print(dts)
