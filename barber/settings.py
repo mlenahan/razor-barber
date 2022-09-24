d7441bf830bbeeeb6912eb1eaddd13b04f191d15
Michael Lenahan <michael_lenahan@hotmail.com>
2022-09-24 15:30:05 +0000

    heroku push error fix to force text error

--- barber/settings.py
+++ barber/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import dj_database_url
 
+import django
+from django.utils.encoding import force_str
+django.utils.encoding.force_text = force_str
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
