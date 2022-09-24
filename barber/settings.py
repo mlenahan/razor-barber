cf0ea17ab4d85631fc3a46a792adcaed89495a92
Michael Lenahan <michael_lenahan@hotmail.com>
2022-09-24 14:21:48 +0000

    Deployment part 2

--- barber/settings.py
+++ barber/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+seki)#gme9k-cn8#-f%*ka(b_2fribfbp3*o9$tdwgmb-or#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://barber-django-ml.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -118,16 +118,17 @@ WSGI_APPLICATION = 'barber.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-# DATABASES = {
-#     'default': dj_database_url.parse('postgres://ekfoefajdfgqbx:f1d10aa9e5bd9cf3c88dc46f2f9ffb0e84ffb564d7ad2e1f28e3e0f83eb1a6d8@ec2-54-170-90-26.eu-west-1.compute.amazonaws.com:5432/d53rbkl6lvpoj')
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
